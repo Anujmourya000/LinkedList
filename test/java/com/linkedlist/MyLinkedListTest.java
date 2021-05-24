24038e15b560703df1fad161bd51266a50a96996
Anujmourya000 <Anujmourya000@gmail.com>
2021-05-24 13:30:08 +0530

    Added: Append Program is added

--- test/java/com/linkedlist/MyLinkedListTest.java
+++ test/java/com/linkedlist/MyLinkedListTest.java
@@ -21,4 +21,20 @@ public class MyLinkedListTest {
         Assertions.assertTrue(result);
 
     }
+
+    @Test
+    public void given_3Numbers_When_Appended_To_LinkedList_ShouldBe_Added_To_Top() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.append(mySecondNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.printMyNode();
+        boolean result = myLinkedList.head.equals(myFirstNode) &&
+                myLinkedList.head.getNext().equals(mySecondNode) &&
+                myLinkedList.tail.equals(myThirdNode);
+        Assertions.assertTrue(result);
+    }
 }
